db3d364cf37ab3f32deb5e06a3721fe5e812fadd
zayke <1659991623@qq.com>
2022-07-05 16:50:07 +0800

    完成了主页的制作

---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+$(function(){
+  getUserInfo()
+
+  // 获取页面的用户信息
+  function getUserInfo(){
+    $.ajax({
+      method: "GET",
+      url:'/my/userinfo', 
+      // headers: {
+      //   Authorization:localStorage.getItem('token')
+      // },
+      success: function(res){
+        console.log(res);
+        if(res.status !==0){
+          return layer.msg('获取用户的基本信息')
+        }
+        
+        renderAvatar(res.data)
+      }
+    })
+  }
+
+  // 渲染用户信息
+  function renderAvatar(user){
+    const name=user.nickname || user.username
+    // user.user_pic='./assets/images/sample.jpg'
+    $('#welcome').html(name)
+    if( user.user_pic ===null){
+      $('.layui-nav-img').hide()
+      const first=name[0].toUpperCase()
+      $('.text-avatar').html(first)
+    }else{
+      $('.text-avatar').hide()
+      $('.layui-nav-img').attr('src',user.user_pic)
+    }
+    
+  }
+
+  // 退出的效果
+  $('#butlogout').on('click', function(){
+    layer.confirm('是否确认退出？', {icon: 3, title:'退出提示'}, function(index){
+      
+      localStorage.removeItem('token')
+      location.href='login.html'
+      layer.close(index);
+    });
+    
+  })
+
+})
